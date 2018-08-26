b51b9ec57890befeb0b67f186ffc59298ed931bf
Alex Jones <alexedwardjones@gmail.com>
2018-08-26 15:16:40 +0100

    Added login to AuthHandler

--- src/services/auth/handlers/AuthHandler.js
+++ src/services/auth/handlers/AuthHandler.js
@@ -18,6 +18,20 @@ module.exports.register = (event, context) => {
     }));
 };
 
+module.exports.login = (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return login(JSON.parse(event.body))
+    .then(session => ({
+      statusCode: 200,
+      body: JSON.stringify(session)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: { stack: err.stack, message: err.message }
+    }));
+};
+
 function signToken(id) {
   return jwt.sign({ id: id }, process.env.JWT_SECRET, {
     expiresIn: 86400 // expires in 24 hours
@@ -62,4 +76,23 @@ function register(eventBody) {
     )
     .then(user => ({ auth: true, token: signToken(user._id) })); 
     // sign the token and send it back
+}
+
+function login(eventBody) {
+  return User.findOne({ email: eventBody.email })
+    .then(user =>
+      !user
+        ? Promise.reject(new Error('User with that email does not exits.'))
+        : comparePassword(eventBody.password, user.password, user._id)
+    )
+    .then(token => ({ auth: true, token: token }));
+}
+
+function comparePassword(eventPassword, userPassword, userId) {
+  return bcrypt.compare(eventPassword, userPassword)
+    .then(passwordIsValid =>
+      !passwordIsValid
+        ? Promise.reject(new Error('The credentials do not match.'))
+        : signToken(userId)
+    );
 }
