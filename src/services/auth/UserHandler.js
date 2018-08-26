4a7012d9a75ca769567ef4a22a64589a942353f2
Alex Jones <alexedwardjones@gmail.com>
2018-08-26 15:05:08 +0100

    Added UserHandler for getUsers

---
+++ src/services/auth/UserHandler.js
@@ -0,0 +1,27 @@
+// UserHandler.js
+
+const dbConnection = require('../../shared/database/connect');
+const User = require('../../models/User');
+
+module.exports.getUsers = (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return fetchUsers()
+    .then(users => ({
+      statusCode: 200,
+      body: JSON.stringify(users)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: JSON.stringify({ message: err.message })
+    }));
+};
+
+function fetchUsers() {
+  return User.findAll({})
+    .then(users => {
+      console.log(users);
+      return users;
+    })
+    .catch(err => Promise.reject(new Error(err)));
+}
