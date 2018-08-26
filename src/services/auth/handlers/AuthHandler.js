2844f6adff3ffa393da550cd0dc549e1d6f1de6a
Alex Jones <alexedwardjones@gmail.com>
2018-08-26 15:14:13 +0100

    Added helper functions to AuthHandler

--- src/services/auth/handlers/AuthHandler.js
+++ src/services/auth/handlers/AuthHandler.js
@@ -1,16 +1,12 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs-then');
 
-
 const dbConnection = require('../../shared/database/connect');
 const User = require('../../models/User');
 
 module.exports.register = (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  return connectToDatabase()
-    .then(() =>
-      register(JSON.parse(event.body))
-    )
+  return register(JSON.parse(event.body))
     .then(session => ({
       statusCode: 200,
       body: JSON.stringify(session)
@@ -21,3 +17,49 @@ module.exports.register = (event, context) => {
       body: err.message
     }));
 };
+
+function signToken(id) {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: 86400 // expires in 24 hours
+  });
+}
+
+function checkIfInputIsValid(eventBody) {
+  if (
+    !(eventBody.password &&
+      eventBody.password.length >= 7)
+  ) {
+    return Promise.reject(new Error('Password error. Password needs to be longer than 8 characters.'));
+  }
+
+  if (
+    !(eventBody.name &&
+      eventBody.name.length > 5 &&
+      typeof eventBody.name === 'string')
+  ) return Promise.reject(new Error('Username error. Username needs to longer than 5 characters'));
+
+  if (
+    !(eventBody.email &&
+      typeof eventBody.name === 'string')
+  ) return Promise.reject(new Error('Email error. Email must have valid characters.'));
+
+  return Promise.resolve();
+}
+
+function register(eventBody) {
+  return checkIfInputIsValid(eventBody) // validate input
+    .then(() =>
+      User.findOne({ email: eventBody.email }) // check if user exists
+    )
+    .then(user =>
+      user
+        ? Promise.reject(new Error('User with that email exists.'))
+        : bcrypt.hash(eventBody.password, 8) // hash the pass
+    )
+    .then(hash =>
+      User.create({ name: eventBody.name, email: eventBody.email, password: hash }) 
+      // create the new user
+    )
+    .then(user => ({ auth: true, token: signToken(user._id) })); 
+    // sign the token and send it back
+}
