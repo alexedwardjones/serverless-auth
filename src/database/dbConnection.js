71baab9fc863661d0e8f50b9e763b824faafb073
Alex Jones <alexedwardjones@gmail.com>
2018-09-01 16:51:34 +0100

    Added host and name to db connection

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize');
 
-const dbConnection = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
+const dbConnection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'postgres',
   operatorsAliases: false,
+  logging: console.log,
 });
 
 module.exports = dbConnection;
